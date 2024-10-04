1c5f3f94a65db24c4bcd7b8c80782258e19e67e5
kadnelliraju1997 <raju.h.gogi@gmail.com>
2024-10-04 18:09:17 +0530

    RK's 1st Commit

---
+++ src/main/java/ApplicationConstants.java
@@ -0,0 +1,9 @@
+
+
+public interface ApplicationConstants 
+{
+	String key="webdriver.chrome.driver";
+	String value="./software/chromedriver.exe";
+	String pro_path="./AppData/AppData.properties";
+
+}
---
+++ src/test/java/ApplicationConstant.java
@@ -0,0 +1,9 @@
+
+
+public interface ApplicationConstant 
+{
+	String key="webdriver.chrome.driver";
+	String value="./software/chromedriver.exe";
+	String pro_path="./AppData/AppData.properties";
+
+}
