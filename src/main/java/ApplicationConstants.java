6909c53799cdc7260147afe0c0ae8fecb813bcd5
kadnelliraju1997 <raju.h.gogi@gmail.com>
2024-10-05 10:49:41 +0530

    Update ApplicationConstants.java

--- src/main/java/ApplicationConstants.java
+++ src/main/java/ApplicationConstants.java
@@ -5,5 +5,8 @@ public interface ApplicationConstants
 	String key="webdriver.chrome.driver";
 	String value="./software/chromedriver.exe";
 	String pro_path="./AppData/AppData.properties";
+	String key1="webdriver.chrome.driver";
+	String value1="./software/chromedriver.exe";
+	String pro_path1="./AppData/AppData.properties";
 
 }
